f23cb26c0a909c7519972384666e00a38dc83def
Michelle <michelle.gust@ontrackretail.co.uk>
2017-03-07 15:45:24 +0000

    cal not working

--- FirstPythonProject/NewPythonFile.py
+++ FirstPythonProject/NewPythonFile.py
@@ -15,7 +15,7 @@ class regressionclass(unittest.TestCase):
     def test_thameslink_buy_ticket(self):
 
         driver = self.driver
-
+        driver.maximize_window()
         driver.get('https://thameslink.stage.otrl.io/search')
 
         # <Step 1: In Tickets tab, Leaving From box, enter 'Welwyn Garden City'.>
@@ -24,22 +24,21 @@ class regressionclass(unittest.TestCase):
         # <Result 1: 'Welwyn Garden City' is seen in the Leaving From box.>
 
         # <Step 2: In Tickets tab, Going To box, enter 'London Kings Cross'.>
-        elem = driver.find_element_by_xpath("//*[@id='container']/div/div/section/div/div[1]/section[1]/div/ul/li[3]/label/div/input")
-        elem.send_keys("London Kings Cross")
-        elem.send_keys(Keys.RETURN)
+        driver.find_element_by_xpath("//*[@id='container']/div/div/section/div/div[1]/section[1]/div/ul/li[3]/label/div/input").send_keys("London Kings")
+        time.sleep(4)
+        driver.find_element_by_xpath("//*[@id='container']/div/div/section/div/div/section[1]/div/ul/li[3]/label/div/input").click()
         print("run ok")
-        time.sleep(5)
+        time.sleep(4)
         # <Result 2: 'London Kings Cross' is seen in the Going To box.>
 
         # <Step 3: In Tickets tab, click on the Outbound Calendar.>
+        #driver.find_element_by_xpath("//*[@id='container']/div/div/section/div/div[1]/section[2]/div[1]/div[1]/div/div[2]/button").click()
         driver.find_element_by_xpath("//*[@id='container']/div/div/section/div/div[1]/section[2]/div/div[1]/div/div[2]/button").click()
-        time.sleep(5)
+        time.sleep(10)
         # <Result 3: 'The Outbound Calendar is opened.>
 
-        #driver.find_element_by_xpath("//*[@id='container']/div/div/section/div/div[1]/section[2]/div/div[1]/div/div[2]/div/div/div[1]/button[3]").click()
-
         # <Step 4: Click on date March 15 2017 in the Calendar.>
-        driver.find_element_by_xpath("//*[@id='rw_1_calendar__month_2-15']").click()
+        driver.find_element_by_xpath("//*[@id='rw_1_calendar__month_2-20']/span").click()
         time.sleep(5)
         # <Result 4: 'March 15 2017 is selected as the Outbound travel date.>
 
@@ -55,7 +54,7 @@ class regressionclass(unittest.TestCase):
 
         # <Step 7: Click on the 'Find trains' button.>
         driver.find_element_by_xpath("//*[@id='container']/div/div/section/div/footer/button").click()
-        time.sleep(15)
+        time.sleep(10)
         # <Result 7: The 'Choose an outbound service' list of trains is displayed.>
 
         # <Step 8: Click on the second train service down from the top.>
@@ -75,24 +74,34 @@ class regressionclass(unittest.TestCase):
 
         # <Step 11: Click on the 'Continue' button.>
         driver.find_element_by_xpath("//*[@id='container']/div/div/section/div/div[3]/div[2]/button[2]").click()
-        time.sleep(10)
+        time.sleep(5)
         # <Result 11: The journey is added to the basket and the 'Order summary' is displayed.>
 
         # <Step 12: Click on '1st Class Mail' in 'Collection preferences'.>
-        driver.find_element_by_xpath("//*[@id='container']/div/div/section/div/div[1]/section/div[2]/div").click()
-        time.sleep(5)
+        #driver.find_element_by_xpath("//*[@id='container']/div/div/section/div/div[1]/section/div[2]/div").click()
+        #time.sleep(5)
         # <Result 12: '1st Class Mail' option is selected in the list of collection preferences.>
 
-        # <Step 13: Click into the 'First name' field in 'Collection preferences'.>
-        driver.find_element_by_xpath("//*[@id='contactName']").click()
+        # <Step 13: Enter 'Miss Michelle Gust' in the 'First name' field.>
+        driver.find_element_by_xpath("//*[@id='contactName']").send_keys("Miss Michelle Gust")
         time.sleep(5)
-        # <Result 13: The curser is placed in the 'First name' field.>
+        # <Result 13: 'Miss Michelle Gust' is seen in the 'First name' field.>
 
-
-        # <Step 14: Click into the 'First name' field in 'Collection preferences'.>
-        driver.find_element_by_xpath("//*[@id='contactName']").click()
+        # <Step 14: Enter '30 Great Guildford Street' in the 'Address line 1' field.>
+        driver.find_element_by_xpath("//*[@id='addressline1']").send_keys("30 Great Guildford Street")
         time.sleep(5)
-        # <Result 13: The curser is placed in the 'First name' field.>
+        # <Result 14: '30 Great Guildford Street' is seen in the 'Address line 1' field.>
+
+        # <Step 15: Enter 'London' in the 'Town/City' field.>
+        driver.find_element_by_xpath("//*[@id='towncity']").send_keys("London")
+        time.sleep(5)
+        # <Result 15: 'London' is seen in the 'Town/City' field.>
+
+        # <Step 16: Enter 'London' in the 'Postcode' field.>
+        driver.find_element_by_xpath("//*[@id='postcode']").send_keys("SE1 0HS")
+        time.sleep(5)
+        # <Result 16: 'London' is seen in the 'Postcode' field.>
+
 
     def tearDown(self):
         self.driver.quit()
